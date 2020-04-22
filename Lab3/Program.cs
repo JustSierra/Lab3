ce670fa4ee59c14984393e45094588bafd22ac12
Sierra Johnson <Bash0704>
2020-04-22 16:56:51 -0400

    Lab3

---
+++ Lab3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            string name = Console.ReadLine();
+            bool Asking = true;
+            while (Asking == true)
+            {
+                Console.WriteLine("enter a number between 1 and 100");
+                string input = Console.ReadLine();
+                int num = int.Parse(input);
+
+                bool even = IsEven(num);
+                if (num >= 1 && num <= 100)
+                {
+
+                }
+            }    }
+}
